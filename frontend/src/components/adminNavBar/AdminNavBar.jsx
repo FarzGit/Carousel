29eb3d2f365b021312bd9b4807be932ae048bd4c
Rahil <rahilsardar234@gmail.com>
2024-02-25 18:44:29 +0530

    removed unwanted text zzz in nav bar

--- frontend/src/components/adminNavBar/AdminNavBar.jsx
+++ frontend/src/components/adminNavBar/AdminNavBar.jsx
@@ -16,7 +16,6 @@ function AdminNavBar(){
                     <div className="h-[65px] w-[100%] flex justify-center items-center space-x-4">
                         <Link to="/admin-list" className="font-medium cursor-pointer" >Home</Link>
                         <Link to="/image-Upload" className="font-medium cursor-pointer">Upload</Link>
-zzzz
                     </div>
 
                 </div>
